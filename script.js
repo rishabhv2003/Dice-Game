d466bd8b1f09e4afae4d9d0139f249fc9315cee9
Rishabh Verma <2000rishabhverma@gmail.com>
2022-12-23 22:33:19 +0530

    players movement

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 var randomNumber1, randomNumber2
-var image1, source;
+var image1, source, check1 = 1, check2 = -1;
+
+
 
 function func1() {
+    if (check1 == -1 && check2 == 1) {
+        alert("you can not move player 1");
+        return;
+    }
     randomNumber1 = Math.floor(Math.random() * 6) + 1;
     source = "images/dice" + randomNumber1 + ".png";
     document.getElementsByClassName('img1')[0].setAttribute('src', source);
+    check1 = -1;
+    check2 = 1;
 }
 
 function func2() {
+    if (check1 == 1 && check2 == -1) {
+        alert("you can not move player 2");
+        return;
+    }
     randomNumber2 = Math.floor(Math.random() * 6) + 1;
     source = "images/dice" + randomNumber2 + ".png";
     document.getElementsByClassName('img2')[0].setAttribute('src', source);
@@ -20,4 +32,6 @@ function func2() {
     else {
         document.getElementsByClassName('result')[0].innerHTML = "Player 2 wins";
     }
+    check1 = 1;
+    check2 = -1;
 }
